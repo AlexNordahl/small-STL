0cb5a9aa9fb4659fb938a707988e5d6d98aff4cc
AlexNordahl <nordahlalex@gmail.com>
2025-10-19 20:03:45 +0200

    created first iteration of vector class

--- project-headers/vector.h
+++ project-headers/vector.h
@@ -1,9 +1,45 @@
-#include <iostream>
+#ifndef small_stl_vector
+#define small_stl_vector
 
 namespace sSTL
 {
-    void hello()
+    template<typename T>
+    class vector
     {
-        std::cout << "Hello stl\n";
+    public:
+        vector() = default;
+        vector(size_t size);
+        vector(size_t size, T init);
+        
+        T& at(size_t index) { return m_array[index]; };
+        T& operator[] (size_t index) { return at(index); };
+
+        size_t size() const { return m_size; };
+        size_t capacity() const { return m_capacity; };
+
+        ~vector() { delete[] m_array; };
+
+    private:
+        size_t m_size {};
+        size_t m_capacity {};
+        T* m_array {};
+        static constexpr size_t max_size {2'000'000};
+    };
+
+    template<typename T>
+    vector<T>::vector(size_t size)
+        : m_size {size}, m_capacity {size}
+    {
+        m_array = new T[m_size];
+    }
+
+    template<typename T>
+    vector<T>::vector(size_t size, T init)
+        : vector(size)
+    {
+        for (size_t i = 0; i < m_size; ++i)
+            m_array[i] = init;
     }
 }
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
 #include "../project-headers/vector.h"
 
 int main()
 {
-    sSTL::hello();
+
+    sSTL::vector<int> vec2 {10};
+
+    for (size_t i = 0; i < vec2.size(); ++i)
+    {
+        std::cout << vec2.at(i) << " ";
+    }
+    std::cout << "\n";    
+
+    sSTL::vector<int> vec3(10, 4);
+
+    for (size_t i = 0; i < vec3.size(); ++i)
+    {
+        std::cout << vec3[i] << " ";
+    }
+    std::cout << "\n";
 }
