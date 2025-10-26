373d1fadd1310b4b1803437d6b6eed5e3a58e97f
AlexNordahl <nordahlalex@gmail.com>
2025-10-26 20:13:50 +0100

    finished non-array type unique_ptr

--- project-headers/unique_ptr.h
+++ project-headers/unique_ptr.h
@@ -9,15 +9,61 @@ namespace sSTL
     class unique_ptr
     {
     public:
-        unique_ptr(T* resource)
+        unique_ptr(T* resource = nullptr) { m_resource = resource; }
+
+        unique_ptr(const unique_ptr&) = delete;
+        unique_ptr& operator=(const unique_ptr&) = delete;
+
+        unique_ptr(const unique_ptr&& other)
         {
+            m_resource = other.m_resource;
+            other.m_resource = nullptr;
+        }
+
+        unique_ptr& operator= (unique_ptr&& other)
+        {
+            if (this == &other)
+                return *this;
+
+            clear();
+
+            m_resource = other.m_resource;
+            other.m_resource = nullptr;
+
+            return *this;
+        }
+
+        T* release()
+        {
+            T* temp = m_resource;
+            m_resource = nullptr;
+            return temp;
+        }
+
+        void reset(T* resource)
+        {
+            clear();
             m_resource = resource;
         }
 
-        ~unique_ptr()
+        void clear()
         {
             Deleter deleter;
-            deleter(resource);
+            deleter(m_resource);
+        }
+
+        T* get() { return m_resource; }
+
+        bool isHolding() { return get() != nullptr;}
+
+        explicit operator bool() { return isHolding(); }
+
+        T& operator* () { return *m_resource; }
+        T* operator-> () { return m_resource; }
+
+        ~unique_ptr()
+        {
+            clear();
         }
     private:
         T* m_resource {};
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,16 @@
 #include "../project-headers/vector.h"
 #include "../project-headers/array.h"
 #include "../project-headers/unique_ptr.h"
-
-int main()
+ 
+int main() 
 {
-    sSTL::unique_ptr<int> (new int (10));
+    sSTL::unique_ptr<int> ptr1 (new int {10});
+    sSTL::unique_ptr<int> ptr2 (new int {4});
+    std::cout << "ptr1: " << *ptr1.get() << "\n";
+    std::cout << "ptr2: " << *ptr2.get() << "\n";
+
+    ptr2 = std::move(ptr1);
+    
+    std::cout << "ptr1: " << ptr1.get() << "\n";
+    std::cout << "ptr2: " << *ptr2.get() << "\n";
 }
