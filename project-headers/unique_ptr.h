92de831cd4203423055f04202e407a695e2950f4
AlexNordahl <nordahlalex@gmail.com>
2025-10-31 16:49:36 +0100

    added array type specialization for unique_ptr and tests

--- project-headers/unique_ptr.h
+++ project-headers/unique_ptr.h
@@ -6,7 +6,7 @@
 namespace sSTL
 {
     template<typename T, typename Deleter = std::default_delete<T>>
-    class unique_ptr final
+    class unique_ptr
     {
     public:
         unique_ptr(T* resource = nullptr) { m_resource = resource; }
@@ -40,7 +40,7 @@ namespace sSTL
             return temp;
         }
 
-        void reset(T* resource)
+        void reset(T* resource = nullptr)
         {
             clear();
             m_resource = resource;
@@ -57,13 +57,78 @@ namespace sSTL
         
         ~unique_ptr() { clear(); }
 
-        private:
+    private:
         T* m_resource {};
 
-        void clear() // Leaves m_resource in unspecified state
+        void clear()
         {
             Deleter deleter;
             deleter(m_resource);
+            m_resource = nullptr;
+        }
+    };
+
+    template<typename T, typename Deleter>
+    class unique_ptr<T[], Deleter>
+    {
+    public:
+        unique_ptr(T* resource = nullptr) { m_resource = resource; }
+
+        unique_ptr(const unique_ptr&) = delete;
+        unique_ptr& operator=(const unique_ptr&) = delete;
+
+        unique_ptr(unique_ptr&& other)
+        {
+            m_resource = other.m_resource;
+            other.m_resource = nullptr;
+        }
+
+        unique_ptr& operator= (unique_ptr&& other)
+        {
+            if (this == &other)
+                return *this;
+
+            clear();
+
+            m_resource = other.m_resource;
+            other.m_resource = nullptr;
+
+            return *this;
+        }
+
+        T* release()
+        {
+            T* temp = m_resource;
+            m_resource = nullptr;
+            return temp;
+        }
+
+        void reset(T* resource = nullptr)
+        {
+            clear();
+            m_resource = resource;
+        }
+        
+        T* get() { return m_resource; }
+        
+        bool isHolding() { return get() != nullptr;}
+        
+        explicit operator bool() { return isHolding(); }
+        
+        T& operator* () { return *m_resource; }
+        T* operator-> () { return m_resource; }
+        T& operator[] (int index) { return m_resource[index]; }
+
+        ~unique_ptr() { clear(); }
+
+    private:
+        T* m_resource {};
+
+        void clear()
+        {
+            Deleter deleter;
+            deleter(m_resource);
+            m_resource = nullptr;
         }
     };
 }
--- tests/unique_ptr_tests.cpp
+++ tests/unique_ptr_tests.cpp
@@ -63,3 +63,107 @@ TEST(UniquePtrMethods, Get)
     EXPECT_EQ(*uptr.get(), 10);
     EXPECT_EQ(*number_ptr, 10);
 }
+
+TEST(UniquePtrArrayConstructor, BasicConstructorNullptr)
+{
+    sSTL::unique_ptr<int[]> uptr{};
+    EXPECT_EQ(uptr.get(), nullptr);
+}
+
+TEST(UniquePtrArrayConstructor, BasicConstructorValues)
+{
+    sSTL::unique_ptr<int[]> uptr { new int[3]{1, 2, 3} };
+
+    ASSERT_NE(uptr.get(), nullptr);
+    EXPECT_EQ(uptr[0], 1);
+    EXPECT_EQ(uptr[1], 2);
+    EXPECT_EQ(uptr[2], 3);
+}
+
+TEST(UniquePtrArrayMoveSemantics, MoveConstructor)
+{
+    sSTL::unique_ptr<int[]> uptr1 { new int[3]{1, 2, 3} };
+    sSTL::unique_ptr<int[]> uptr2 { std::move(uptr1) };
+
+    EXPECT_EQ(uptr1.get(), nullptr);
+    ASSERT_NE(uptr2.get(), nullptr);
+    EXPECT_EQ(uptr2[0], 1);
+    EXPECT_EQ(uptr2[1], 2);
+    EXPECT_EQ(uptr2[2], 3);
+}
+
+TEST(UniquePtrArrayMoveSemantics, MoveAssignmentOperator)
+{
+    sSTL::unique_ptr<int[]> uptr1 { new int[2]{10, 20} };
+    sSTL::unique_ptr<int[]> uptr2 {};
+    uptr2 = std::move(uptr1);
+
+    EXPECT_EQ(uptr1.get(), nullptr);
+    ASSERT_NE(uptr2.get(), nullptr);
+    EXPECT_EQ(uptr2[0], 10);
+    EXPECT_EQ(uptr2[1], 20);
+}
+
+TEST(UniquePtrArrayMethods, Release)
+{
+    sSTL::unique_ptr<int[]> uptr { new int[2]{10, 20} };
+
+    int* arr_ptr = uptr.release();
+
+    EXPECT_EQ(uptr.get(), nullptr);
+    ASSERT_NE(arr_ptr, nullptr);
+    EXPECT_EQ(arr_ptr[0], 10);
+    EXPECT_EQ(arr_ptr[1], 20);
+
+    delete[] arr_ptr;
+}
+
+TEST(UniquePtrArrayMethods, ResetToNewArray)
+{
+    sSTL::unique_ptr<int[]> uptr { new int[3]{1, 2, 3} };
+
+    uptr.reset(new int[3]{4, 5, 6});
+
+    ASSERT_NE(uptr.get(), nullptr);
+    EXPECT_EQ(uptr[0], 4);
+    EXPECT_EQ(uptr[1], 5);
+    EXPECT_EQ(uptr[2], 6);
+}
+
+TEST(UniquePtrArrayMethods, ResetToNullptr)
+{
+    sSTL::unique_ptr<int[]> uptr { new int[1]{42} };
+
+    uptr.reset(nullptr);
+
+    EXPECT_EQ(uptr.get(), nullptr);
+}
+
+TEST(UniquePtrArrayMethods, Get)
+{
+    sSTL::unique_ptr<int[]> uptr { new int[3]{7, 8, 9} };
+
+    int* raw = uptr.get();
+
+    ASSERT_NE(raw, nullptr);
+    EXPECT_EQ(uptr[0], 7);
+    EXPECT_EQ(uptr[1], 8);
+    EXPECT_EQ(uptr[2], 9);
+
+    EXPECT_EQ(raw[0], 7);
+    EXPECT_EQ(raw[1], 8);
+    EXPECT_EQ(raw[2], 9);
+}
+
+TEST(UniquePtrArrayMethods, IndexingAfterMove)
+{
+    sSTL::unique_ptr<int[]> uptr1 { new int[4]{11, 22, 33, 44} };
+    sSTL::unique_ptr<int[]> uptr2 { std::move(uptr1) };
+
+    EXPECT_EQ(uptr1.get(), nullptr);
+    ASSERT_NE(uptr2.get(), nullptr);
+    EXPECT_EQ(uptr2[0], 11);
+    EXPECT_EQ(uptr2[1], 22);
+    EXPECT_EQ(uptr2[2], 33);
+    EXPECT_EQ(uptr2[3], 44);
+}
